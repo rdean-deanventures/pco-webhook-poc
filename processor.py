6fab16af2cf20bc8d4f39710bee94be6a1d77958
Rick Dean <ric.dean@gmail.com>
2026-06-12 14:14:17 -0400

    Processor Step 2 - inspect payload

--- processor.py
+++ processor.py
@@ -1,12 +1,22 @@
+import json
+
+
 def process_webhook(payload):
 
     print("\n=================================")
     print("PROCESSOR STARTED")
     print("=================================")
 
-    print("Processor was called successfully.")
+    print(f"Payload Type: {type(payload)}")
 
-    print("=================================")
+    try:
+        print("\nFULL PAYLOAD:")
+        print(json.dumps(payload, indent=4))
+    except Exception as e:
+        print(f"Could not format JSON: {e}")
+        print(payload)
+
+    print("\n=================================")
     print("PROCESSOR COMPLETE")
     print("=================================\n")
 
