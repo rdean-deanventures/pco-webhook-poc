9023ccc99306e636322c663bae6e38299ec43681
Rick Dean <ric.dean@gmail.com>
2026-06-12 11:36:40 -0400

    Route webhook to processor function

--- processor.py
+++ processor.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 
-# 1. Just for email:
-import smtplib
-from email.message import EmailMessage
+
 
 def handle_webhook_event(payload):
     """
@@ -20,24 +18,3 @@ def handle_webhook_event(payload):
     print("===== END PROCESSOR =====\n")
 
 
-############### EMAIL SECTION ######################
-
-# 2. Create the email message
-msg = EmailMessage()
-msg["Subject"] = "Script Notification"
-msg["From"] = "rdean@itownchurch.com"
-msg["To"] = "rdean@itownchurch.com"
-msg.set_content("Hello, Your PCO webhook kicked off your python script.")
-
-# 3. Connect to the server and send it
-try:
-    # This example uses Gmail's SMTP server configuration
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
-        server.starttls()  # Upgrade the connection to secure
-        server.login("rdean@itownchurch.com", "ugsl ovll mydp nckr") # If running from Google Cloud, place these in a password protector.
-        server.send_message(msg)
-    print("Email notification sent successfully!")
-except Exception as e:
-    print(f"Failed to send email. Error: {e}")
-
-####################################################
